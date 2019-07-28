d4343a041ac68223e82e188a8dec515ea3eb34d5
jhb115 <peterbyun96@gmail.com>
2019-07-28 14:47:35 +0100

    Context ComplEx

--- kbc/models_ctx.py
+++ kbc/models_ctx.py
@@ -74,10 +74,160 @@ class KBCModel(nn.Module, ABC):
 
 Sigmoid = nn.Sigmoid()
 
+class ComplEx(KBCModel):
+    def __init__(
+            self, sizes: Tuple[int, int, int], rank: int, sorted_data:np.ndarray,
+            slice_dic: np.ndarray, max_NB: int=50, init_size: float=1e-3,
+            data_name: str='FB15K'
+    ):
+        super(ComplEx, self).__init__()
+        n_s, n_r, n_o = sizes
+        self.sizes = [n_s, n_r, n_o, n_o]  #append another n_o for nb_o
+        self.rank = rank
+
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(s, 3 * rank, sparse=True)
+            for s in self.sizes[:3]
+        ])
+        self.embeddings[0].weight.data *= init_size
+        self.embeddings[1].weight.data *= init_size
+        self.embeddings[2].weight.data *= init_size  # For context
+
+        # self.W  # need to consider the dimensionality
+        # self.W2  # need to consider the dimensionality
+
+        self.drop_layer1 = nn.Dropout(p=0.3)
+        self.drop_layer2 = nn.Dropout(p=0.3)
+
+        # self.Wo
+        # self.Uo
+
+        # nn.init.xavier_uniform_(self.W.weight)
+        # nn.init.xavier_uniform_(self.W2.weight)
+
+        self.sorted_data = sorted_data
+        self.slice_dic = slice_dic
+        self.max_NB = max_NB
+
+        self.forward_g = []
+        self.valid_g = []
+
+        self.i = 0
+
+    def get_neighbor(self, subj: torch.Tensor):
+        # return neighbor (N_subject, N_nb_max, k)
+
+        index_array = np.zeros(shape=(len(subj), self.max_NB), dtype=np.int32)
+
+        for i, each_subj in enumerate(subj):
+            _, start_i, end_i = self.slice_dic[each_subj]
+            length = end_i - start_i
+
+            if length > 0:
+                if self.max_NB > length:
+                    index_array[i, :length] = self.sorted_data[start_i:end_i, 2]
+                else:
+                    index_array[i, :] = self.sorted_data[start_i:start_i+self.max_NB, 2]
+
+        # Convert index_array into a long tensor for indexing the embedding.
+        index_tensor = torch.LongTensor(index_array).cuda()
+
+        return self.embeddings[2](index_tensor)
+
+    def score(self, x: torch.Tensor):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+
+        # Concatenation of lhs, rel
+        trp_E = torch.cat((lhs[0], rel[0]), dim=1), torch.cat((lhs[1], rel[1]), dim=1)
+
+        # Get attention weight vector, linear projection of trp_E
+        # w = self.W(trp_E)
 
 
 
 
+
+        # Get nb_E
+        #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def score(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+
+        return torch.sum(
+            (lhs[0] * rel[0] - lhs[1] * rel[1]) * rhs[0] +
+            (lhs[0] * rel[1] + lhs[1] * rel[0]) * rhs[1],
+            1, keepdim=True
+        )
+
+    def forward(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+
+        to_score = self.embeddings[0].weight
+        to_score = to_score[:, :self.rank], to_score[:, self.rank:]
+        return (
+            (lhs[0] * rel[0] - lhs[1] * rel[1]) @ to_score[0].transpose(0, 1) +
+            (lhs[0] * rel[1] + lhs[1] * rel[0]) @ to_score[1].transpose(0, 1)
+        ), (
+            torch.sqrt(lhs[0] ** 2 + lhs[1] ** 2),
+            torch.sqrt(rel[0] ** 2 + rel[1] ** 2),
+            torch.sqrt(rhs[0] ** 2 + rhs[1] ** 2)
+        )
+
+    def get_rhs(self, chunk_begin: int, chunk_size: int):
+        return self.embeddings[0].weight.data[
+            chunk_begin:chunk_begin + chunk_size
+        ].transpose(0, 1)
+
+    def get_queries(self, queries: torch.Tensor):
+        lhs = self.embeddings[0](queries[:, 0])
+        rel = self.embeddings[1](queries[:, 1])
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+
+        return torch.cat([
+            lhs[0] * rel[0] - lhs[1] * rel[1],
+            lhs[0] * rel[1] + lhs[1] * rel[0]
+        ], 1)
 
 
 
