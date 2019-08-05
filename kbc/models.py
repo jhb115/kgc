ed7e5f17679c57d13e831aaf5ca373479f34ee9e
jhb115 <peterbyun96@gmail.com>
2019-08-05 15:31:53 +0100

    Update

--- kbc/models.py
+++ kbc/models.py
@@ -146,11 +146,18 @@ class Context_CP(KBCModel):
             length = end_i - start_i
 
             if length > 0:
-                if self.max_NB > length:
-                    index_array[i, :length] = self.sorted_data[start_i:end_i: self.ascending, 2]
+                if self.ascending == 1:
+                    if self.max_NB >= length:
+                        index_array[i, :length] = self.sorted_data[start_i:end_i, 2]
+                    else:  # Need to uniformly truncate
+                        hop = int(length / self.max_NB)
+                        index_array[i, :] = self.sorted_data[start_i:start_i + self.max_NB:hop, 2]
                 else:
-                    hop = int(length / self.max_NB) * self.ascending
-                    index_array[i, :] = self.sorted_data[start_i:start_i + self.max_NB:hop, 2]
+                    if self.max_NB >= length:
+                        index_array[i, :length] = self.sorted_data[end_i:start_i:self.ascending, 2]
+                    else:  # Need to uniformly truncate
+                        hop = int(length / self.max_NB) * self.ascending
+                        index_array[i, :] = self.sorted_data[start_i + self.max_NB:start_i:hop, 2]
 
         # Convert index_array into a long tensor for indexing the embedding.
         index_tensor = torch.LongTensor(index_array).cuda()
