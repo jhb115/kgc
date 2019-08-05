0b6648f4b038be116e6d3c4ccdbdecb40d060faf
jhb115 <peterbyun96@gmail.com>
2019-08-05 15:39:56 +0100

    Update

--- kbc/models.py
+++ kbc/models.py
@@ -602,13 +602,13 @@ class Context_ComplEx(KBCModel):
                         index_array[i, :length] = self.sorted_data[start_i:end_i, 2]
                     else:  # Need to uniformly truncate
                         hop = int(length / self.max_NB)
-                        index_array[i, :] = self.sorted_data[start_i:start_i+self.max_NB:hop, 2]
+                        index_array[i, :] = self.sorted_data[start_i:start_i+self.max_NB:hop, 2][:self.max_NB]
                 else:
                     if self.max_NB >= length:
                         index_array[i, :length] = self.sorted_data[end_i:start_i:self.ascending, 2]
                     else:  # Need to uniformly truncate
                         hop = int(length / self.max_NB) * self.ascending
-                        index_array[i, :] = self.sorted_data[start_i+self.max_NB:start_i:hop, 2]
+                        index_array[i, :] = self.sorted_data[start_i+self.max_NB:start_i:hop, 2][:self.max_NB]
 
         index_tensor = torch.LongTensor(index_array).cuda()
 
