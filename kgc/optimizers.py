ea6773b54a21a7ab196de08ee4921b183f5bd12a
jhb115 <peterbyun96@gmail.com>
2019-09-17 11:59:14 +0100

    update

--- kgc/optimizers.py
+++ kgc/optimizers.py
@@ -52,32 +52,32 @@ class KBCOptimizer(object):
         actual_examples = examples[torch.randperm(examples.shape[0]), :]
         loss = nn.CrossEntropyLoss(reduction='mean')
 
-        with tqdm.tqdm(total=examples.shape[0], unit='ex', disable=not self.verbose) as bar:
-            bar.set_description(f'train loss')
-            b_begin = 0
-            while b_begin < examples.shape[0]:
-                input_batch = actual_examples[
-                    b_begin:b_begin + self.batch_size
-                ].cuda()
+        # with tqdm.tqdm(total=examples.shape[0], unit='ex', disable=not self.verbose) as bar:
+        # bar.set_description(f'train loss')
+        b_begin = 0
+        while b_begin < examples.shape[0]:
+            input_batch = actual_examples[
+                b_begin:b_begin + self.batch_size
+            ].cuda()
 
-                predictions, factors = self.model.forward(input_batch)
-                truth = input_batch[:, 2]
+            predictions, factors = self.model.forward(input_batch)
+            truth = input_batch[:, 2]
 
-                l_fit = loss(predictions, truth)
-                if self.regularizer == 'N0':
-                    l = l_fit
-                elif self.model.context_flag:
-                    l_reg = self.regularizer.forward(factors, g=self.model.g)
-                    l = l_fit + l_reg
-                else:
-                    l_reg = self.regularizer.forward(factors)
-                    l = l_fit + l_reg
+            l_fit = loss(predictions, truth)
+            if self.regularizer == 'N0':
+                l = l_fit
+            elif self.model.context_flag:
+                l_reg = self.regularizer.forward(factors, g=self.model.g)
+                l = l_fit + l_reg
+            else:
+                l_reg = self.regularizer.forward(factors)
+                l = l_fit + l_reg
 
-                self.optimizer.zero_grad()
-                l.backward()
-                self.optimizer.step()
-                b_begin += self.batch_size
-                bar.update(input_batch.shape[0])
-                bar.set_postfix(loss=f'{l.item():.0f}')
+            self.optimizer.zero_grad()
+            l.backward()
+            self.optimizer.step()
+            b_begin += self.batch_size
+            # bar.update(input_batch.shape[0])
+            # bar.set_postfix(loss=f'{l.item():.0f}')
 
-                self.loss_list.append(l.clone().data.cpu().numpy())
+            self.loss_list.append(l.clone().data.cpu().numpy())
